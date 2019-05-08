8f0185689b496c7056a45f49e653b33d481ec977
anaisTe <angieanais@gmail.com>
2019-05-07 19:15:23 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// let movies = ['Star Wars', 'Game of Thrones', 'Lord of the Rings', 'Harry Potter'];
+// let randomNumb = Math.floor((Math.random()* movies.length-1)+1);
+// let randMovie = randMovie[randomNumb];
+
+// const appCall = () => {
+//   console.log(randMovie)
+// }
+
---
+++ isVowel.spec.js
@@ -0,0 +1,47 @@
+/*const isVowel = require('./isVowel');
+
+console.log('debería devolver true para letra a');
+if (isVowel('a') === true) {
+  console.log('✓ ok');
+} else {
+  console.error('✗ fail');
+}
+
+console.log('debería devolver false para letra b');
+if (isVowel('b') === false) {
+  console.log('✓ ok');
+} else {
+  console.error('✗ fail');
+}
+
+describe('isVowel()', () => {
+    it('debería devolver true para letra a', ()=>{
+        if (isVowel('a') !== true) {
+            console.error('✗ fail');
+        } else {
+            console.log('✓ ok');
+        }
+    });
+    it('debería devolver false para letra b', ()=>{
+        if (isVowel('b') !== false) {
+            console.error('✗ fail');
+        } else {
+            console.log('✓ ok');
+        }
+    });
+  });*/
+//aserciones
+// const assert = require('assert');
+// const isVowel = require('./isVowel');
+
+// describe('isVowel()', () => {
+
+//   it('debería devolver true para letra a', () => {
+//     assert.equal(isVowel('a'), true);
+//   });
+
+//   it('debería devolver false para letra a', () => {
+//     assert.equal(isVowel('b'), false);
+//   });
+
+// });
